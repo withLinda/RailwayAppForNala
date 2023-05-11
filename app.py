eb94279b09df1eadf79f04e032260ca5818ec6e6
Linda <linda.fitriani@live.com>
2023-05-11 23:07:17 +0700

    4th commit: added meta_tags

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import pandas as pd
 from dash import Dash, dash_table, dcc, html, Input, Output, State
 
 df = pd.read_csv('profile_data1.csv')  # Read the csv file
-app = Dash(__name__)
+app = Dash(__name__,
+           meta_tags=[{'name': 'viewport',
+                       'content': 'width=device-width, initial-scale=1.0'}]
+           )
 server = app.server
 
 dtable = dash_table.DataTable(  # Create the table
